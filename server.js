fb29632da91461e5172b1b0030dc3c5a351c4eef
Anas Alqoyyum <doublezero13@gmail.com>
2021-03-21 18:48:51 +0700

    A

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
+const corsOptions = {
+  origin: "*",
+  allowedHeaders: "*",
+  optionsSuccessStatus: 200,
+  method: "GET ,HEAD ,PUT ,PATCH, POST, DELETE, OPTIONS",
+};
+app.use(cors(corsOptions));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "Selamat datang di omorphia API." });
 });
-const port = 3000;
+const port = 3001;
 require("./app/routes/contact.routes")(app);
 app.listen(port, () => console.log(`Server ini berjalan dalam port 3000`));
